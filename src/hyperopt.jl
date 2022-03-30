e8fc300f3a88b8e1626fae84ab2ef26265477b3c
Max <maximilian.gelbrecht@posteo.de>
2022-03-30 16:15:37 +0200

    merge results

--- src/hyperopt.jl
+++ src/hyperopt.jl
@@ -1,39 +1,63 @@
 
 import Base.push!, Base.getindex
+using JLD2
+
+Base.@kwdef struct HyperoptResults{S,T,V}
+    pars::S
+    res::T 
+    additonal_res::V = nothing
+end
+
+res(r::HyperoptResults) = r.res 
+pars(r::HyperoptResults) = r.pars
 
 """ 
     SlurmHyperoptimizer(ho::Hyperoptimizer; slurm_params::SlurmParams=nothing)
 
 Creates a Slurm Job Array submission file and a Hyperoptimizer instance for your Hyperoptimizer needs. Takes the same arguments as Hyperoptimizer from Hyperopt + the keyword argument slurm_params.
 """
-mutable struct SlurmHyperoptimizer{S,T,V}
+mutable struct SlurmHyperoptimizer
     sampler::AbstractHyperparameterSampler
-    pars::S 
-    res::T
-    additional_res::V
+    results::Vector{HyperoptResults}
     N_samples::Integer
     slurm_params::SlurmParams
+    temp_dir
 end
 
-
-function SlurmHyperoptimizer(N_samples::Integer, sampler::AbstractHyperparameterSampler, slurm_params::SlurmParams)
+function SlurmHyperoptimizer(N_samples::Integer, sampler::AbstractHyperparameterSampler, slurm_params::SlurmParams, temp_dir="temp/")
     generate_slurm_file(slurm_params, N_samples)    
-    SlurmHyperoptimizer(sampler, [], [], [], N_samples, slurm_params)
+    mkpath(temp_dir)
+    results = Vector{HyperoptResults}(undef, N_samples)
+    SlurmHyperoptimizer(sampler, results, N_samples, slurm_params, temp_dir)
 end
 
 """
-    push!(ho::SlurmHyperoptimizer, pars, res, additonal_res=nothing)
+    save_result(sho::SlurmHyperoptimizer, res::HyperoptResults, i::Integer)
 
-Adds the results to the SlurmHyperoptimizer object add the end of each task.
+(Temporally) saves the results `res` of job `i`. Results are later merged with `merge_results!`.
+"""
+function save_result(sho::SlurmHyperoptimizer, res::HyperoptResults, i::Integer)
+    save_path = string(sho.temp_dir,"res-",i,".jld2")
+    JLD2.@save save_path res 
+end 
 
 """
-function push!(ho::SlurmHyperoptimizer, pars, res, additonal_res=nothing)
-    push!(ho.pars, pars)
-    push!(ho.res, res)
-    push!(ho.additional_res, additonal_res)
-    ho
+    merge_results!(sho::SlurmHyperoptimizer)
+
+Merges the results and store them in the `sho.results` field. Also deletes the temporal files. 
+"""
+function merge_results!(sho::SlurmHyperoptimizer)
+    for i ∈ 1:sho.N_samples
+        save_path = string(sho.temp_dir,"res-",i,".jld2")
+        JLD2.@load save_path res 
+        sho.results[i] = res 
+    end 
+    rm(sho.temp_dir, recursive=true)
+    return sho
 end
 
+get_results(sho::SlurmHyperoptimizer) = res.(sho.results)
+get_params(sho::SlurmHyperoptimizer) = pars.(sho.results)
 
 """
     get_index(ho::SlurmHyperoptimizer, i::Integer)
