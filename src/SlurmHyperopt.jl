d3a85d225c90eb00544c67f7f4cc7c796a6fc152
Max <maximilian.gelbrecht@posteo.de>
2022-03-30 17:21:14 +0200

    export

--- src/SlurmHyperopt.jl
+++ src/SlurmHyperopt.jl
@@ -5,6 +5,6 @@ abstract type AbstractHyperparameterSampler end
 include("slurm_file.jl")
 include("hyperopt.jl")
 
-export SlurmParams, SlurmHyperoptimizer, RandomSampler
+export SlurmParams, SlurmHyperoptimizer, RandomSampler, HyperoptResults
 
 end
