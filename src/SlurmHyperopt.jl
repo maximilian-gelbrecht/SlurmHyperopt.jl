a9d994786995dbde6230c287e4b1e8c4afa04edc
Max <maximilian.gelbrecht@posteo.de>
2024-06-27 12:41:06 +0200

    export ProductSampler, bump CI

--- src/SlurmHyperopt.jl
+++ src/SlurmHyperopt.jl
@@ -5,7 +5,7 @@ abstract type AbstractHyperparameterSampler end
 include("slurm_file.jl")
 include("hyperopt.jl")
 
-export SlurmParams, SlurmHyperoptimizer, RandomSampler, HyperoptResults
+export SlurmParams, SlurmHyperoptimizer, RandomSampler, ProductSampler, HyperoptResults
 export merge_results!, get_params, get_results, save_result
 
 end
